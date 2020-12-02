485038d996f4e64dc3ccd11cf4ff323b66af69df
Parthvi Vala <pvala@redhat.com>
2020-12-02 20:15:06 +0530

    Day2 part 2

--- day2/main.go
+++ day2/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strconv"
 	"strings"
-	"io/ioutil"
 )
 
-func getData() []string{
+func getData() []string {
 	file, err := ioutil.ReadFile("input.txt")
-	if err!=nil {
+	if err != nil {
 		panic(err)
 	}
 	seperatedString := strings.Split(string(file), "\n")
@@ -19,19 +19,27 @@ func getData() []string{
 func main() {
 	var policy, limit []string
 	var password, subject string
-	var min, max, subjectCount, counter int
+	var position1, position2, subjectCount, counter int
 	// inputs := []string{"1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"}
 	inputs := getData()
 	for _, input := range inputs {
 		getPolicyPW := strings.SplitAfter(input, ": ")
 		policy, password = strings.Split(getPolicyPW[0], " "), strings.TrimSpace(getPolicyPW[1])
 		limit, subject = strings.Split(policy[0], "-"), strings.Trim(policy[1], ": ")
-		min, _ = strconv.Atoi(limit[0])
-		max, _ = strconv.Atoi(limit[1])
+		position1, _ = strconv.Atoi(limit[0])
+		position2, _ = strconv.Atoi(limit[1])
 
 		subjectCount = strings.Count(password, subject)
-		if subjectCount >= min && subjectCount <= max {
-			counter++
+		position1char := string(password[position1-1])
+		position2char := string(password[position2-1])
+		if subjectCount == 1 {
+			if position1char == subject || position2char == subject {
+				counter++
+			}
+		} else if subjectCount > 1 {
+			if (position1char == subject && position2char != subject) || (position1char != subject && position2char == subject) {
+				counter++
+			}
 		}
 	}
 	fmt.Println(counter)
